ec63311d9629ad66b58af2768feff1a8a9f379c8
Hunter Lewis <53382814+HunterLewis000@users.noreply.github.com>
2025-08-13 21:42:57 -0500

    git updateee

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-# app.py
 from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO, emit
 import os
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://newspaper_db_47wk_user:2WQbescUw19AeDpYVPPGZzFeVnyePdiV@dpg-d2e1sv3e5dus73feem00-a.ohio-postgres.render.com/newspaper_db_47wk'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@host:port/dbname'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+socketio = SocketIO(app, cors_allowed_origins="*")  # Enable cross-origin for Render
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +21,7 @@ def index():
     articles = Article.query.all()
     return render_template('index.html', articles=articles)
 
+# Add Article
 @app.route('/add', methods=['POST'])
 def add_article():
     title = request.form['title']
@@ -29,17 +30,28 @@ def add_article():
     new_article = Article(title=title, author=author, deadline=deadline)
     db.session.add(new_article)
     db.session.commit()
+    # Emit the new article to all connected clients
+    socketio.emit('article_added', {
+        'id': new_article.id,
+        'title': title,
+        'author': author,
+        'status': new_article.status,
+        'deadline': deadline
+    })
     return redirect('/')
 
+# Delete Article
 @app.route('/delete/<int:article_id>', methods=['POST'])
 def delete_article(article_id):
     article = Article.query.get(article_id)
     if article:
         db.session.delete(article)
         db.session.commit()
+        socketio.emit('article_deleted', {'id': article_id})
         return jsonify(success=True)
     return jsonify(success=False), 404
 
+# Update Article
 @app.route('/update/<int:article_id>', methods=['POST'])
 def update_article(article_id):
     article = Article.query.get(article_id)
@@ -48,18 +60,25 @@ def update_article(article_id):
         article.title = data.get('title', article.title)
         article.author = data.get('author', article.author)
         article.deadline = data.get('deadline', article.deadline)
-        article.status = data.get('status', article.status)
         db.session.commit()
+        socketio.emit('article_updated', {
+            'id': article.id,
+            'title': article.title,
+            'author': article.author,
+            'deadline': article.deadline
+        })
         return jsonify(success=True)
     return jsonify(success=False), 404
 
-
+# Update Status
 @app.route('/update_status/<int:article_id>', methods=['POST'])
 def update_status(article_id):
     article = Article.query.get(article_id)
     if article:
-        article.status = request.json.get('status')
+        new_status = request.json.get('status')
+        article.status = new_status
         db.session.commit()
+        socketio.emit('status_updated', {'id': article_id, 'status': new_status})
         return jsonify(success=True)
     return jsonify(success=False), 404
 
@@ -67,5 +86,4 @@ if __name__ == '__main__':
     if not os.path.exists('newspaper.db'):
         with app.app_context():
             db.create_all()
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
-
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
