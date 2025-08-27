ae3c80bad9f65cb7c89a4bcf45321d7399b5fdbf
Hunter Lewis <53382814+HunterLewis000@users.noreply.github.com>
2025-08-27 16:33:10 -0500

    event popup

--- app.py
+++ app.py
@@ -409,6 +409,9 @@ def calendar_events():
         'title': e.get('summary', 'No Title'),
         'start': e.get('start', {}).get('dateTime') or e.get('start', {}).get('date'),
         'end': e.get('end', {}).get('dateTime') or e.get('end', {}).get('date')
+        'description': e.get('description', ''),
+        'location': e.get('location', ''),
+        'creator': creator.get('displayName', '')
     } for e in data.get('items', [])]
     return jsonify(events)
 
